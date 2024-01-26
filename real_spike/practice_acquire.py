d54a210f2df0b2894dbaa16e8b8d4825cf4d9506
Caitlin Lewis <clewis7@email.unc.edu>
2024-01-26 12:13:15 -0500

    update practice aquisition functions

--- real_spike/practice_acquire.py
+++ real_spike/practice_acquire.py
@@ -2,11 +2,12 @@ from sglx_pkg import sglx as sglx
 from ctypes import byref, POINTER, c_int, c_short, c_bool, c_char_p
 from time import time
 
+
 def connect(ip_address: str, port: int):
-    print("\nCalling connect...\n\n")
+    """Check that connection can be made to SpikeGLX running on aquisition machine."""
+    print("Calling connect...\n\n")
     hSglx = sglx.c_sglx_createHandle()
 
-    # Using default loopback address and port
     if sglx.c_sglx_connect(hSglx, ip_address.encode(), port):
         print("Successfully connected to SpikeGLX")
         print("version <{}>\n".format(sglx.c_sglx_getVersion(hSglx)))
@@ -16,99 +17,163 @@ def connect(ip_address: str, port: int):
     sglx.c_sglx_close(hSglx)
     sglx.c_sglx_destroyHandle(hSglx)
 
-def get_params(ip_address: str, port: int):
-    print( "\nGet params...\n\n" )
+
+def check_inits(ip_address: str, port: int):
+    """Check initial conditions of SpikeGLX before attempting to fetch."""
     hSglx = sglx.c_sglx_createHandle()
-    ok    = sglx.c_sglx_connect(hSglx, ip_address.encode(), port)
+    ok = sglx.c_sglx_connect(hSglx, ip_address.encode(), port)
 
     if ok:
+        ready = c_bool()
+        ok = sglx.c_sglx_isInitialized(byref(ready), hSglx)
+        if not ok:
+            print("SpikeGLX has NOT completed its startup initialization")
+
+            sglx.c_sglx_close(hSglx)
+            sglx.c_sglx_destroyHandle(hSglx)
+            return
+        else:
+            print("SpikeGLX has completed startup initialization, checking if running...")
+            # check if running
+            running = c_bool()
+            ok = sglx.c_sglx_isRunning(byref(running), hSglx)
+            if ok:
+                print("SpikeGLX is running")
+                sglx.c_sglx_close(hSglx)
+                sglx.c_sglx_destroyHandle(hSglx)
+                return
+
+    if not ok:
+        print("error [{}]\n".format(sglx.c_sglx_getError(hSglx)))
+
+    sglx.c_sglx_close(hSglx)
+    sglx.c_sglx_destroyHandle(hSglx)
+
+
+def get_params(ip_address: str, port: int):
+    print("Get params...")
+    hSglx = sglx.c_sglx_createHandle()
+    ok = sglx.c_sglx_connect(hSglx, ip_address.encode(), port)
+
+    if ok:
+        print("Successfully connected to SpikeGLX")
         nval = c_int()
-        len  = c_int()
-        ok   = sglx.c_sglx_getParams(byref(nval), hSglx)
+        len = c_int()
+        ok = sglx.c_sglx_getParams(byref(nval), hSglx)
         if ok:
             kv = {}
             for i in range(0, nval.value):
                 line = sglx.c_sglx_getstr(byref(len), hSglx, i).decode()
-                parts = line.split( '=' )
-                kv.update( {parts[0]: parts[1]} )
-            print( "{}".format( kv.items() ) )
+                parts = line.split('=')
+                kv.update({parts[0]: parts[1]})
+            print("{}".format(kv.items()))
 
     if not ok:
-        print( "error [{}]\n".format(sglx.c_sglx_getError(hSglx)))
+        print("error [{}]\n".format(sglx.c_sglx_getError(hSglx)))
 
     sglx.c_sglx_close(hSglx)
     sglx.c_sglx_destroyHandle(hSglx)
+
+
+def get_datadir(ip_address: str, port: int):
+    hSglx = sglx.c_sglx_createHandle()
+    ok = sglx.c_sglx_connect(hSglx, ip_address.encode(), port)
+
+    if ok:
+        data_dir = c_char_p()
+        ok = sglx.c_sglx_getDataDir(byref(data_dir), hSglx, 0)
+
+        if ok:
+            print(data_dir.value)
+        if not ok:
+            print("error [{}]\n".format(sglx.c_sglx_getError(hSglx)))
+
+            sglx.c_sglx_close(hSglx)
+            sglx.c_sglx_destroyHandle(hSglx)
+            return
+
+    if not ok:
+        print("error [{}]\n".format(sglx.c_sglx_getError(hSglx)))
+
+    sglx.c_sglx_close(hSglx)
+    sglx.c_sglx_destroyHandle(hSglx)
+
 
 def get_probes(ip_address: str, port: int):
     hSglx = sglx.c_sglx_createHandle()
-    sglx.c_sglx_connect(hSglx, ip_address.encode(), port)
-
-    l = c_char_p()
-
-    ok = sglx.c_sglx_getProbeList(byref(l), hSglx)
+    ok = sglx.c_sglx_connect(hSglx, ip_address.encode(), port)
 
     if ok:
-        print(l.value)
-
+        list = c_char_p()
+        ok = sglx.c_sglx_getProbeList(byref(list), hSglx)
+        if ok:
+            print(list.value)
     if not ok:
-        print("bah")
+        print("error [{}]\n".format(sglx.c_sglx_getError(hSglx)))
 
     sglx.c_sglx_close(hSglx)
     sglx.c_sglx_destroyHandle(hSglx)
 
-def fetch_data(ip_address: str, port: int):
+
+def get_imec_params(ip_address: str, port: int, ip=0):
     hSglx = sglx.c_sglx_createHandle()
-    sglx.c_sglx_connect(hSglx, ip_address.encode(), port)
-
-    running = POINTER(c_bool)()
-    ok = sglx.c_sglx_isRunning(running, hSglx)
-
-    if ok:
-        print("yay")
-
-    sglx.c_sglx_close(hSglx)
-    sglx.c_sglx_destroyHandle(hSglx)
-    return
-
-    js = 2
-    ip = 0
-
-    #np = sglx.c_sglx_getStreamNP(,hSglx, js)
-
-    fromCt = sglx.c_sglx_getStreamSampleCount(hSglx, js, ip)
-
-    # srate = sglx.c_sglx_getStreamSampleRate(hSglx, js, ip)
-    # print(srate)
-
-    #if srate > 0:
-    data = POINTER(c_short)()
-    ndata = c_int()
-    py_chans = [i for i in range(64)]
-    nC = len(py_chans)
-    channels = (c_int * nC)(*py_chans)
-
-    t = time()
-    headCt = sglx.c_sglx_fetch(byref(data), byref(ndata), hSglx, js, ip, fromCt, 120, channels, nC, 1)
-    print(time()-t)
-
-    print(byref(data))
-
-    sglx.c_sglx_close(hSglx)
-    sglx.c_sglx_destroyHandle(hSglx)
-
-
-def getShankMap_test(ip_address, port):
-    print( "\nGet shank map...\n\n" )
-    hSglx = sglx.c_sglx_createHandle()
-    ok    = sglx.c_sglx_connect(hSglx, ip_address.encode(), port)
+    ok = sglx.c_sglx_connect(hSglx, ip_address.encode(), port)
 
     if ok:
         nval = c_int()
-        len = c_int()
-        ok = sglx.c_sglx_getNIShankMap(byref(nval), hSglx)
+        ok = sglx.c_sglx_getParamsImecProbe(byref(nval), hSglx, ip)
         if ok:
+            kv = {}
             for i in range(0, nval.value):
-                print("{}".format(sglx.c_sglx_getstr(byref(len), hSglx, i)))
+                line = sglx.c_sglx_getstr(byref(len), hSglx, i).decode()
+                parts = line.split('=')
+                kv.update({parts[0]: parts[1]})
+            print("{}".format(kv.items()))
+    if not ok:
+        print("error [{}]\n".format(sglx.c_sglx_getError(hSglx)))
+
+    sglx.c_sglx_close(hSglx)
+    sglx.c_sglx_destroyHandle(hSglx)
+
+
+def fetch_data(ip_address: str, port: int, ip=0):
+    hSglx = sglx.c_sglx_createHandle()
+    ok = sglx.c_sglx_connect(hSglx, ip_address.encode(), port)
+
+    if ok:
+        js = 2
+        data = POINTER(c_short)()
+        ndata = c_int()
+
+        # collect from first 60 channels on the probe
+        py_chans = [i for i in range(60)]
+
+        # number of channels getting data from
+        n_cs = len(py_chans)
+
+        # channel_subset is an array of specific channels to fetch, optionally,
+        #      -1 = all acquired channels, or,
+        #      -2 = all saved channels.
+        channel_subset = (c_int * n_cs)(*py_chans)
+
+        # downsample = every nth sample
+        downsample = 1
+
+        # get number of samples since current run started
+        # use as start sample
+        fromCt = sglx.c_sglx_getStreamSampleCount(hSglx, js, ip)
+
+        if fromCt > 0:
+            headCt = sglx.c_sglx_fetch(byref(data), byref(ndata), hSglx, js, ip, fromCt, max_samps, channel_subset,
+                                       n_cs, downsample)
+
+            if headCt == 0:
+                print("error [{}]\n".format(sglx.c_sglx_getError(hSglx)))
+                sglx.c_sglx_close(hSglx)
+                sglx.c_sglx_destroyHandle(hSglx)
+                return
+
+            print(data.value)
 
     if not ok:
         print("error [{}]\n".format(sglx.c_sglx_getError(hSglx)))
@@ -118,13 +183,15 @@ def getShankMap_test(ip_address, port):
 
 
 if __name__ == "__main__":
-    # connect the acquisition machine
-    #connect(ip_address="192.168.0.101", port=4142)
+    # practice connection
+    connect(ip_address="192.168.0.101", port=4142)
+    # get parameters
+    get_params(ip_address="192.168.0.101", port=4142)
+    # get the main data_dir
+    get_datadir(ip_address="192.168.0.101", port=4142)
     # get probes
-    #get_probes(ip_address="192.168.0.101", port=4142)
-    # get params
-    #get_params(ip_address="192.168.0.101", port=4142)
+    get_probes(ip_address="192.168.0.101", port=4142)
+    # get imec probe params for a given probe
+    get_imec_params(ip_address="192.168.0.101", port=4142, ip=0)
     # fetch data
-    fetch_data(ip_address="192.168.0.101", port=4142)
-    #getShankMap_test(ip_address="192.168.0.101", port=4142)
-
+    fetch_data(ip_address="192.168.0.101", port=4142, ip=0)
