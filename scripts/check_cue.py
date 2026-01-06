1a8d66ace3f41bf70327d8d3b068f588b604b805
clewis7 <caitlin9165@gmail.com>
2026-01-06 16:41:41 -0500

    lint and add dep

--- scripts/check_cue.py
+++ scripts/check_cue.py
@@ -9,8 +9,8 @@ cue_signal = Event()
 
 CUE_NUM = 0
 
-port_name = '/dev/ttyACM0'
-baud_rate = 9600 # Check your device's documentation for the correct baud rate
+port_name = "/dev/ttyACM0"
+baud_rate = 9600  # Check your device's documentation for the correct baud rate
 
 ser = serial.Serial(port=port_name, baudrate=baud_rate, timeout=1)
 print("opened port")
@@ -21,9 +21,9 @@ while True:
     bytes_available = ser.in_waiting
 
     if bytes_available > 0:
-        line = ser.readline().decode(errors='ignore').strip()
-        if line == '1':
+        line = ser.readline().decode(errors="ignore").strip()
+        if line == "1":
             CUE_NUM += 1
             # Trigger received
             print(f"RECEIVED CUE {CUE_NUM}")
-           # ser.reset_input_buffer()
+        # ser.reset_input_buffer()
